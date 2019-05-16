f6466da5eb9d4f6e3be7539c031ad3cbffc9ff39
FrazileDevelopers <frazile.com@gmail.com>
2019-05-16 12:34:24 +0530

    AMOLED

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(AMOLED());
+
+class AMOLED extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => MaterialApp();
+}
