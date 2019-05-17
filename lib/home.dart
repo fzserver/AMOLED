87f4c63020dec1244d1d74bb6714a40d73b22b8c
FrazileDevelopers <frazile.com@gmail.com>
2019-05-17 23:41:57 +0530

    code updated

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,6 @@
-
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AMOLEDHome extends StatefulWidget {
   @override
@@ -27,24 +28,36 @@ class _AMOLEDHomeState extends State<AMOLEDHome> {
     super.dispose();
   }
 
-  List<String> wallpapersList = [
-    'https://images.pexels.com/photos/2252299/pexels-photo-2252299.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    'https://images.pexels.com/photos/2272829/pexels-photo-2272829.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    'https://images.pexels.com/photos/2291725/pexels-photo-2291725.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-  ];
+  StreamSubscription<QuerySnapshot> subscription;
+  List<DocumentSnapshot> wallpapersList;
+
+  void firestore() async {
+    final Firestore firestore = Firestore();
+    await firestore.settings(timestampsInSnapshotsEnabled: true);
+    final CollectionReference cr = firestore.collection('amoled');
+    subscription = cr
+        .snapshots()
+        .listen((data) => setState(() => wallpapersList = data.documents));
+  }
+
+  // List<String> wallpapersList = [
+  //   'https://images.pexels.com/photos/2252299/pexels-photo-2252299.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+  //   'https://images.pexels.com/photos/2272829/pexels-photo-2272829.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+  //   'https://images.pexels.com/photos/2291725/pexels-photo-2291725.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+  // ];
 }
 
-Widget wallList(BuildContext ctx, List<String> images) => images != null
-    ? GridView.count(
-        crossAxisCount: 2,
-        childAspectRatio: .64,
-        children: List.generate(
-          images.length,
-          (index) => Image(
-                image: NetworkImage(images[index]),
-                fit: BoxFit.cover,
-               
-              ),
-        ),
-      )
-    : Center();
+Widget wallList(BuildContext ctx, List<DocumentSnapshot> images) =>
+    images != null
+        ? GridView.count(
+            crossAxisCount: 2,
+            childAspectRatio: .64,
+            children: List.generate(
+              images.length,
+              (index) => Image(
+                    image: NetworkImage(images[index].data['url']),
+                    fit: BoxFit.cover,
+                  ),
+            ),
+          )
+        : Center();
